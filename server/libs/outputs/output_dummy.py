de5e3d66b2b4bfa82e11e7f07358fc99ece091ec
Anton <goldtom1423@gmail.com>
2021-02-06 20:47:32 +0100

    Format output as PEP style

--- server/libs/outputs/output_dummy.py
+++ server/libs/outputs/output_dummy.py
@@ -1,10 +1,10 @@
-from libs.outputs.output import Output # pylint: disable=E0611, E0401
+from libs.outputs.output import Output  # pylint: disable=E0611, E0401
+
 
 class OutputDummy(Output):
     def __init__(self, device):
-
         # Call the constructor of the base class.
         super(OutputDummy, self).__init__(device)
 
     def show(self, output_array):
-        print("Output dummy..")
+        print("Output dummy...")
