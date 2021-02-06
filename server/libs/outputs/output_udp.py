1703b6c927682ff75eba39bc606815cfbb70e442
Anton <goldtom1423@gmail.com>
2021-02-06 20:47:32 +0100

    Format output as PEP style

--- server/libs/outputs/output_udp.py
+++ server/libs/outputs/output_udp.py
@@ -1,11 +1,11 @@
-from libs.outputs.output import Output # pylint: disable=E0611, E0401
+from libs.outputs.output import Output  # pylint: disable=E0611, E0401
 from time import sleep
 import numpy as np
 import socket
 
+
 class OutputUDP(Output):
     def __init__(self, device):
-
         # Call the constructor of the base class.
         super(OutputUDP, self).__init__(device)
 
@@ -16,8 +16,7 @@ class OutputUDP(Output):
         self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def show(self, output_array):
-
-        # Typecast the array to int
+        # Typecast the array to int.
         output_array = output_array.clip(0, 255).astype(np.uint8)
-        byte_array = output_array.tobytes('F')      
+        byte_array = output_array.tobytes('F')
         self._sock.sendto(byte_array, (self._udp_client_ip, self._udp_client_port))
