ecd3a34cb4c7bbb8c8803f8583c571eb91fef981
Anton <goldtom1423@gmail.com>
2021-02-20 19:36:00 +0100

    Check Python version on start (#74)

--- server/libs/config_service.py
+++ server/libs/config_service.py
@@ -5,10 +5,12 @@
 
 from logging.handlers import RotatingFileHandler
 from shutil import copyfile, copy
+from sys import version_info
 from pathlib import Path
 import coloredlogs
 import logging
 import json
+import sys
 import os
 
 
@@ -17,6 +19,10 @@ class ConfigService():
         self.setup_logging()
 
         self.logger = logging.getLogger(__name__)
+
+        if version_info < (3, 6):
+            self.logger.error("Error: MLSC requires Python 3.6 or greater.")
+            sys.exit()
 
         config_file = "config.json"
         config_backup_file = "config_backup.json"
