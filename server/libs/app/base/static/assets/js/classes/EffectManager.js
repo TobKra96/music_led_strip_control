fa6b1a95291a755ccafa09f45c8acd1fc5fe99df
weiserhei <7581732+weiserhei@users.noreply.github.com>
2021-04-13 17:44:51 +0100

    implement EffectManager Class

---
+++ server/libs/app/base/static/assets/js/classes/EffectManager.js
@@ -0,0 +1,127 @@
+// classes/EffectManager.js
+export class EffectManager {
+    constructor() {
+        // todo: refactor server-side
+        this.nonMusicEffects = $("#dashboard-list-none-music > div > div").map(function() { return this.id }).toArray();
+        this.musicEffects = $("#dashboard-list-music > div > div").map(function() { return this.id }).toArray();
+        this.specialEffects = $("#dashboard-list-special > div > div").map(function () { return this.id }).toArray();
+        this.currentDevice;
+
+        // Listen for effect change on click
+        this.allEffects.forEach(effect => {
+            $("#" + effect).on('click', () => {
+                this.switchEffect(effect);
+            });
+        });
+
+        initTimerWorker();
+    }
+
+    get allEffects () {
+        return this.nonMusicEffects.concat(this.musicEffects, this.specialEffects)
+    }
+    
+    get allLightEffects() {
+        return this.nonMusicEffects.concat(this.musicEffects)
+    }
+
+    getRandomEffect(type, activeEffect) {
+        let pool, randomEffect;
+
+        if (type == 'effect_random_non_music') {
+            pool = this.nonMusicEffects;
+        }else if (type == 'effect_random_music') {
+            pool = this.musicEffects;
+        } else {
+            pool = this.allLightEffects;
+        }
+
+        do {
+            randomEffect = pool[Math.floor(Math.random() * pool.length)];
+        } while(randomEffect === activeEffect)
+        return randomEffect;
+    }
+
+    switchEffect(effect) {
+        if (effect.length > 0) {
+
+            if (effect == 'effect_random_cycle') {
+                const effectCycleActive = sessionStorage.getItem('effect_cycle_active');
+                if (!effectCycleActive) {
+                    timer.postMessage({
+                        seconds: hardcodedSec,
+                        status: 'start'
+                    });
+                    sessionStorage.setItem('effect_cycle_active', true);
+                    $("#effect_random_cycle > div").addClass("border border-secondary");
+                }
+            } else {
+                timer.postMessage({
+                    seconds: 0,
+                    status: 'stop'
+                });
+                sessionStorage.clear();
+                $("#effect_random_cycle > div > p").text("Random Cycle");
+                $("#effect_random_cycle > div").removeClass("border border-secondary");
+            }
+            
+            // pick random effect based on type
+            if(effect == "effect_random_cycle" || effect == "effect_random_non_music" || effect == "effect_random_music") {
+                effect = this.getRandomEffect(effect, this.currentDevice.activeEffect);
+            }
+
+            $.ajax({
+                url: "/SetActiveEffect",
+                type: "POST",
+                data: JSON.stringify({ "device": this.currentDevice.id, "effect": effect }),
+                contentType: 'application/json;charset=UTF-8'
+            }).done((data) => {
+                // UI and State Updates should be here
+                // this could cause Problems later
+            }).fail((data) => {
+                $("#alerts").append(`
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>(`+ new Date().toLocaleTimeString() +`) Error: </strong> `+data.responseText+`
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                </div>`);
+            });
+            // update UI without waiting for a response
+            this.currentDevice.setActiveEffect(effect);
+        }
+    }
+
+}
+
+let timer;
+const hardcodedSec = 10;
+
+function initTimerWorker() {
+    timer = new Worker('/static/assets/js/timer.js');
+
+    // Get messages from timer worker
+    timer.onmessage = (event) => {
+        var sec = event.data;
+        sessionStorage.setItem('seconds', sec);
+        $("#effect_random_cycle > div > p").text("Random Cycle (" + sec + "s)");
+        if (sec <= 0) {
+            sessionStorage.clear();
+            $("#effect_random_cycle")[0].click();
+        }
+    };
+    
+    // Restore timer if it was running while page reloaded
+    var effectCycleActive = sessionStorage.getItem('effect_cycle_active');
+    if (effectCycleActive) {
+        var sec = sessionStorage.getItem('seconds');
+        if (sec <= 0) {
+            sec = hardcodedSec;
+            sessionStorage.setItem('seconds', sec);
+        }
+        timer.postMessage({
+            seconds: sec,
+            status: 'start'
+        });
+    }
+}
--- server/libs/app/base/static/assets/js/dashboard.js
+++ server/libs/app/base/static/assets/js/dashboard.js
@@ -1,127 +1,17 @@
 import { Device } from "./classes/Device.js";
-
-/* Effect Handling */
-
-function getRandomEffect(effects, activeEffect) {
-    do {
-        var randomEffect = effects[Math.floor(Math.random() * effects.length)];
-    } while(randomEffect === activeEffect)
-    return randomEffect;
-}
-function switchEffect(e) {
-    let newActiveEffect = "";
-    let BreakException = {};
-
-    // wat (pls refactor)
-    try {
-        var path = e.path || (e.composedPath && e.composedPath());
-        path.forEach(element => {
-            if (element.classList != null) {
-                if (element.classList.contains("dashboard_effect")) {
-                    newActiveEffect = element.id;
-                    throw BreakException;
-                }
-            }
-        });
-    }
-    catch (e) {
-        if (e !== BreakException) throw e;
-    }
-    // wat (pls refactor)
-
-    if (newActiveEffect.length > 0) {
-        console.log(newActiveEffect + " was clicked.");
-
-        if (newActiveEffect == 'effect_random_cycle') {
-            const effectCycleActive = sessionStorage.getItem('effect_cycle_active');
-            if (!effectCycleActive) {
-                timer.postMessage({
-                    seconds: hardcodedSec,
-                    status: 'start'
-                });
-                sessionStorage.setItem('effect_cycle_active', true);
-            }
-            newActiveEffect = getRandomEffect(allEffects, currentDevice.activeEffect);
-        } else {
-            timer.postMessage({
-                seconds: 0,
-                status: 'stop'
-            });
-            sessionStorage.clear();
-            $("#effect_random_cycle > div > p").text("Random Cycle");
-        }
-
-        if (newActiveEffect == 'effect_random_non_music') {
-            newActiveEffect = getRandomEffect(allNonMusicEffects, currentDevice.activeEffect);
-        }
-        if (newActiveEffect == 'effect_random_music') {
-            newActiveEffect = getRandomEffect(allMusicEffects, currentDevice.activeEffect);
-        }
-        $.ajax({
-            url: "/SetActiveEffect",
-            type: "POST",
-            data: JSON.stringify({ "device": currentDevice.id, "effect": newActiveEffect }),
-            contentType: 'application/json;charset=UTF-8'
-        }).done((data) => {
-            currentDevice.setActiveEffect(newActiveEffect);
-            console.table("Effect set successfully. Response:\n\n" + JSON.stringify(data, null, '\t'));
-        }).fail((data) => {
-            console.log("Error while setting effect. Error: " + data.responseText);
-        });
-    }
-}
-
-// Listen for effect change on click
-document.getElementById("dashboard-item-list").addEventListener("click", function (e) {
-    switchEffect(e);
-});
-
-function initTimerWorker() {
-    timer = new Worker('/static/assets/js/timer.js');
-
-    // Get messages from timer worker
-    timer.onmessage = (event) => {
-        var sec = event.data;
-        sessionStorage.setItem('seconds', sec);
-        $("#effect_random_cycle > div > p").text("Random Cycle (" + sec + "s)");
-        if (sec <= 0) {
-            sessionStorage.clear();
-            $("#effect_random_cycle")[0].click();
-        }
-    };
-    
-    // Restore timer if it was running while page reloaded
-    var effectCycleActive = sessionStorage.getItem('effect_cycle_active');
-    if (effectCycleActive) {
-        var sec = sessionStorage.getItem('seconds');
-        if (sec <= 0) {
-            sec = hardcodedSec;
-            sessionStorage.setItem('seconds', sec);
-        }
-        timer.postMessage({
-            seconds: sec,
-            status: 'start'
-        });
-    }
-}
+import { EffectManager } from "./classes/EffectManager.js";
 
 // Global Variables
+const effectManager = new EffectManager();
 
 // Start with Fake Device
 const devices = [new Device("all_devices", "All Devices")];
 let currentDevice = devices[0];
-let timer;
-const hardcodedSec = 10;
-// todo: refactor server-side
-const allNonMusicEffects = $("#dashboard-list-none-music > div > div").map(function() { return this.id }).toArray();
-const allMusicEffects = $("#dashboard-list-music > div > div").map(function() { return this.id }).toArray();
-const allEffects = allNonMusicEffects.concat(allMusicEffects);
 
 // Init and load all settings
 $(document).ready(function () {
 
     $.ajax({ url: "/GetDevices" }).done((data) => {
-
         // data = { device_0: "devicename1", device_1: "devicename2" }
         // todo: return anon Objects from Endpoint
 
@@ -141,6 +31,9 @@ $(document).ready(function () {
             // Fallback to all_devices
             currentDevice = devices[0];
         }
+        
+        effectManager.currentDevice = currentDevice;
+
         // Async function
         currentDevice.getActiveEffect();
 
@@ -159,6 +52,7 @@ $(document).ready(function () {
             link.addEventListener('click', () => {
                 currentDevice = device;
                 localStorage.setItem('lastDevice', device.id);
+                effectManager.currentDevice = currentDevice;
                 // Async function
                 device.getActiveEffect();
             });
@@ -168,10 +62,6 @@ $(document).ready(function () {
             li.appendChild(link)
 
             document.getElementById("deviceTabID").appendChild(li);
-
         });
      });
-
-    initTimerWorker();
-
 });
