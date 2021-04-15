7acc18b2dfd982a8fd069ab50b3198539befa821
weiserhei <7581732+weiserhei@users.noreply.github.com>
2021-04-15 21:46:10 +0100

    stack new toasts on top

--- server/libs/app/base/static/assets/js/classes/Toast.js
+++ server/libs/app/base/static/assets/js/classes/Toast.js
@@ -43,7 +43,7 @@ export default class Toast {
             </div>
         `;
 
-        $(".toast_block").append(toast);
+        $(".toast_block").prepend(toast);
         $('.toast').toast('show').on('hidden.bs.toast', function () {
             $(this).remove();
         })
