7bffee528ac37352a19a2597093c4b31b2c291ba
Anton Grouchtchak <goldtom1423@gmail.com>
2021-04-15 03:32:00 +0200

    Fix cycle effect border on small screens

--- server/libs/app/base/static/assets/js/classes/EffectManager.js
+++ server/libs/app/base/static/assets/js/classes/EffectManager.js
@@ -53,7 +53,7 @@ export default class EffectManager {
                         status: 'start'
                     });
                     sessionStorage.setItem('effect_cycle_active', true);
-                    $("#effect_random_cycle > div").css("box-shadow", "inset 0 0 0 3px #3f4d67")
+                    $("#effect_random_cycle").css("box-shadow", "inset 0 0 0 3px #3f4d67")
                 }
             } else {
                 const effectCycleActive = sessionStorage.getItem('effect_cycle_active');
@@ -65,7 +65,7 @@ export default class EffectManager {
                     sessionStorage.clear();
                 }
                 $("#effect_random_cycle > div > p").text("Random Cycle");
-                $("#effect_random_cycle > div").css("box-shadow", "none")
+                $("#effect_random_cycle").css("box-shadow", "none")
             }
 
             // pick random effect based on type
@@ -132,7 +132,7 @@ function initTimerWorker() {
     // Restore timer if it was running while page reloaded
     var effectCycleActive = sessionStorage.getItem('effect_cycle_active');
     if (effectCycleActive) {
-        $("#effect_random_cycle > div").css("box-shadow", "inset 0 0 0 3px #3f4d67")
+        $("#effect_random_cycle").css("box-shadow", "inset 0 0 0 3px #3f4d67")
         var sec = sessionStorage.getItem('seconds');
         if (sec <= 0) {
             sec = hardcodedSec;
