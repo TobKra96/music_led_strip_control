9e0f628f9d6f612ea3717d02a394ed2a477c37f5
Anton Grouchtchak <goldtom1423@gmail.com>
2021-04-30 01:42:04 +0200

    [WIP] Add general settings API documentation

--- server/libs/webserver/blueprints/general_settings_api.py
+++ server/libs/webserver/blueprints/general_settings_api.py
@@ -8,64 +8,172 @@ import json
 general_settings_api = Blueprint('general_settings_api', __name__)
 
 
-@general_settings_api.route('/api/settings/general', methods=['GET', 'POST', 'DELETE'])
+@general_settings_api.route('/api/settings/general', methods=['GET'])
 @login_required
-def general_settings():  # pylint: disable=E0211
-    if request.method == 'GET':
-        # Retrieve settings from config.
-        if len(request.args) == 1:
-            data_in = request.args.to_dict()
-            data_out = copy.deepcopy(data_in)
-
-            if not Executer.instance.general_settings_executer.validate_data_in(data_in, ("setting_key",)):
-                return "Input data are wrong.", 403
-
-            setting_value = Executer.instance.general_settings_executer.get_general_setting(data_in["setting_key"])
-            data_out["setting_value"] = setting_value
-
-            if setting_value is None:
-                return "Could not find settings value: ", 403
-            else:
-                return jsonify(data_out)
-
-        elif len(request.args) == 0:
-            # If no queries passed, return all settings.
-            data_out = dict()
-
-            settings = Executer.instance.general_settings_executer.get_general_settings()
-            data_out["setting_value"] = settings
-
-            if settings is None:
-                return "Could not find settings value: ", 403
-            else:
-                return jsonify(data_out)
-
-        return "Input data are wrong.", 403
-
-    elif request.method == 'POST':
-        # Save new settings to config.
-        data_in = request.get_json()
+def get_general_settings():  # pylint: disable=E0211
+    """
+    Return general settings
+    ---
+    tags:
+        - Settings
+    parameters:
+        - name: setting_key
+          in: query
+          type: string
+          required: false
+          enum: ['default_sample_rate', 'device_id', 'frames_per_buffer', 'log_file_enabled', 'log_level_console', 'log_level_file',
+                 'max_frequency', 'min_frequency', 'min_volume_threshold', 'n_fft_bins', 'n_rolling_history', webserver_port]
+          description: Specific `setting_key` to return from general settings\n
+                       Return all settings if not specified
+    responses:
+        200:
+            description: OK
+            schema:
+                type: object,
+                example:
+                    {
+                        setting_key: str,
+                        setting_value: str
+                    }
+        403:
+            description: Input data are wrong
+    """
+    if len(request.args) == 1:
+        data_in = request.args.to_dict()
         data_out = copy.deepcopy(data_in)
 
-        if not Executer.instance.general_settings_executer.validate_data_in(data_in, ("settings", )):
+        if not Executer.instance.general_settings_executer.validate_data_in(data_in, ("setting_key",)):
             return "Input data are wrong.", 403
 
-        Executer.instance.general_settings_executer.set_general_setting(data_in["settings"])
+        setting_value = Executer.instance.general_settings_executer.get_general_setting(data_in["setting_key"])
+        data_out["setting_value"] = setting_value
 
-        return jsonify(data_out)
+        if setting_value is None:
+            return "Could not find settings value: ", 403
+        else:
+            return jsonify(data_out)
 
-    elif request.method == 'DELETE':
-        # Reset settings in config to default.
+    elif len(request.args) == 0:
+        # If no queries passed, return all settings.
         data_out = dict()
 
-        Executer.instance.general_settings_executer.reset_settings()
+        settings = Executer.instance.general_settings_executer.get_general_settings()
+        data_out["setting_value"] = settings
 
-        return jsonify(data_out)
+        if settings is None:
+            return "Could not find settings value: ", 403
+        else:
+            return jsonify(data_out)
+
+    return "Input data are wrong.", 403
 
 
-@general_settings_api.route('/api/settings/export')
+@general_settings_api.route('/api/settings/general', methods=['POST'])
+@login_required
+def set_general_settings():  # pylint: disable=E0211
+    """
+    Set general settings
+    ---
+    tags:
+        - Settings
+    parameters:
+        - name: data
+          in: body
+          type: string
+          required: true
+          description: The general settings which to set\n
+          schema:
+                type: object,
+                example:
+                    {
+                        settings: {
+                            default_sample_rate: int,
+                            device_id: str,
+                            frames_per_buffer: int,
+                            log_file_enabled: bool,
+                            log_level_console: str,
+                            log_level_file: str,
+                            max_frequency: int,
+                            min_frequency: int,
+                            min_volume_threshold: float,
+                            n_fft_bins: int,
+                            n_rolling_history: int,
+                            webserver_port: int
+                        }
+                    }
+    responses:
+        200:
+            description: OK
+            schema:
+                type: object,
+                example:
+                    {
+                        settings: {
+                            default_sample_rate: int,
+                            device_id: str,
+                            frames_per_buffer: int,
+                            log_file_enabled: bool,
+                            log_level_console: str,
+                            log_level_file: str,
+                            max_frequency: int,
+                            min_frequency: int,
+                            min_volume_threshold: float,
+                            n_fft_bins: int,
+                            n_rolling_history: int,
+                            webserver_port: int
+                        }
+                    }
+        403:
+            description: Input data are wrong
+    """
+    data_in = request.get_json()
+    data_out = copy.deepcopy(data_in)
+
+    if not Executer.instance.general_settings_executer.validate_data_in(data_in, ("settings", )):
+        return "Input data are wrong.", 403
+
+    Executer.instance.general_settings_executer.set_general_setting(data_in["settings"])
+
+    return jsonify(data_out)
+
+
+@general_settings_api.route('/api/settings/general', methods=['DELETE'])
+@login_required
+def reset_general_settings():  # pylint: disable=E0211
+    """
+    Reset general settings
+    ---
+    tags:
+        - Settings
+    responses:
+        200:
+            description: OK
+            schema:
+                type: object,
+                example:
+                    {}
+        403:
+            description: Input data are wrong
+    """
+    data_out = dict()
+
+    Executer.instance.general_settings_executer.reset_settings()
+
+    return jsonify(data_out)
+
+
+@general_settings_api.route('/api/settings/export', methods=['GET'])
 @login_required
 def export_config():  # pylint: disable=E0211
+    """
+    Export configuration file
+    ---
+    tags:
+        - Settings
+    responses:
+        200:
+            description: OK
+    """
     Executer.instance.logger.debug(f"Send file: {Executer.instance.general_settings_executer.export_config_path}")
     return send_file(Executer.instance.general_settings_executer.export_config_path, as_attachment=True, cache_timeout=-1, mimetype="text/html")
 
@@ -73,6 +181,27 @@ def export_config():  # pylint: disable=E0211
 @general_settings_api.route('/api/settings/import', methods=['POST'])
 @login_required
 def import_config():  # pylint: disable=E0211
+    """
+    Import configuration file
+    ---
+    tags:
+        - Settings
+    consumes:
+        - multipart/form-data
+    parameters:
+        - name: imported_config
+          in: formData
+          type: file
+          required: true
+          description: Upload config file.
+    responses:
+        200:
+            description: OK
+        400:
+            description: Could not import file
+        404:
+            description: No config file selected
+    """
     Executer.instance.logger.debug("Import Config Request received.")
     if 'imported_config' not in request.files:
         Executer.instance.logger.error("Could not find the file key.")
