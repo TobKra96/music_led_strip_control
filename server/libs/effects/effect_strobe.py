b8f70739982b141ff218cafe2ea378ec534c330b
Anton <goldtom1423@gmail.com>
2022-05-19 23:49:53 -0400

    Minor fixes to Strobe effect
    * Fix wrong `use_custom_color` type.
    * Fix LED strip cases.
    * Remove unnecessary `super()` parameters.
    * Optimize repeating code.
    * Format comments to match codebase style.
    * Fix typos.
    * Trim whitespace.

--- server/libs/effects/effect_strobe.py
+++ server/libs/effects/effect_strobe.py
@@ -1,7 +1,7 @@
 from libs.effects.effect import Effect  # pylint: disable=E0611, E0401
 
+from time import time
 import numpy as np
-import time
 
 
 class EffectStrobe(Effect):
@@ -9,13 +9,13 @@ class EffectStrobe(Effect):
     def __init__(self, device):
 
         # Call the constructor of the base class.
-        super(EffectStrobe, self).__init__(device)
+        super().__init__(device)
 
-        # needed to calculate the time
-        self.now = time.time() * 1000
-        self.last_time = time.time() * 1000
+        # Used to calculate time between strobe states.
+        self.now = time() * 1000
+        self.last_time = self.now
 
-        # False = OFF, True = ON
+        # False - strobe OFF, True - strobe ON.
         self.current_state = False
 
     def run(self):
@@ -23,23 +23,22 @@ class EffectStrobe(Effect):
         effect_config = self.get_effect_config("effect_strobe")
         self._led_strip = self._device.device_config["led_strip"]
 
-        speed = effect_config.get("speed", 1)
+        speed = effect_config["speed"]
 
-        self.now = time.time() * 1000
+        self.now = time() * 1000
 
         if (self.now - self.last_time) > (1000 / speed):
-            self.last_time = time.time() * 1000
+            self.last_time = time() * 1000
             self.current_state = not self.current_state
 
-        
         # Build an empty array.
-        if "SK6812" in self._led_strip:
+        if "sk6812" in self._led_strip:
             output_array = np.zeros((4, self._device.device_config["led_count"]))
         else:
             output_array = np.zeros((3, self._device.device_config["led_count"]))
 
         if self.current_state:
-            if effect_config.get("use_custom_color"):
+            if effect_config["use_custom_color"]:
                 color = effect_config["custom_color"]
             else:
                 color = self._config_colours[effect_config["color"]]
@@ -47,13 +46,11 @@ class EffectStrobe(Effect):
             color = self._config_colours["black"]
 
         # Fill the array with the selected color.
-        output_array[0][:] = color[0]
-        output_array[1][:] = color[1]
-        output_array[2][:] = color[2]
+        for i in range(3):
+            output_array[i][:] = color[i]
 
-        if "SK6812" in self._led_strip:
+        if "sk6812" in self._led_strip:
             output_array[3][:] = effect_config["white"]
 
         # Add the output array to the queue.
         self.queue_output_array_blocking(output_array)
-
