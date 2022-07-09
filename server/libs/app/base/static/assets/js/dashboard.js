a29e5e9939a290c66cf7fb291ae7d83ef77f73bd
Anton Grouchtchak <goldtom1423@gmail.com>
2022-07-08 20:13:09 -0400

    Add dark mode (#177)
    Default to `prefers-color-scheme`.
    Toggle theme with a button on dashboard.
    Script is inside `head` to avoid FOUC.

--- server/libs/app/base/static/assets/js/dashboard.js
+++ server/libs/app/base/static/assets/js/dashboard.js
@@ -4,6 +4,12 @@ import Toast from "./classes/Toast.js";
 
 $(function () {
 
+    setThemeIcon(localStorage.theme);
+
+    $("#theme-indicator").on("click", () => {
+        toggleTheme();
+    });
+
     if (!jinja_devices.length) {
         new Toast('No device found. Create a new device in "Device Settings".').info();
         return;
@@ -23,3 +29,31 @@ $(function () {
     });
 
 });
+
+/**
+ * Show corresponding theme icon.
+ * @param {string} theme
+ */
+let setThemeIcon = (theme) => {
+    if (theme === "dark") {
+        $("#theme-indicator").addClass("icon-sun");
+        $("#theme-indicator").removeClass("icon-moon");
+    } else {
+        $("#theme-indicator").addClass("icon-moon");
+        $("#theme-indicator").removeClass("icon-sun");
+    }
+}
+
+/**
+ * Toggle theme and save to localStorage.
+ */
+let toggleTheme = () => {
+    if (localStorage.theme === "dark") {
+        $("html").removeClass('dark');
+        localStorage.theme = "light";
+    } else {
+        $("html").addClass('dark');
+        localStorage.theme = "dark";
+    }
+    setThemeIcon(localStorage.theme);
+}
