b64f258470f35e124ec9abb9fc092c895e4154c2
Anton Grouchtchak <goldtom1423@gmail.com>
2022-08-30 01:19:33 -0400

    Fix 404 error page not showing for some URLs

--- server/libs/app/__init__.py
+++ server/libs/app/__init__.py
@@ -3,8 +3,8 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from flask import Flask, render_template
 from importlib import import_module
-from flask import Flask
 
 
 def register_blueprints(app):
@@ -13,7 +13,21 @@ def register_blueprints(app):
         app.register_blueprint(module.blueprint)
 
 
+def page_not_found(_):
+    return render_template('page-404.html'), 404
+
+
+def server_error(_):
+    return render_template('page-500.html'), 500
+
+
+def register_errors(app):
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, server_error)
+
+
 def create_app():
     app = Flask(__name__, static_folder='base/static')
     register_blueprints(app)
+    register_errors(app)
     return app
