53848fa20fc4989f88e2bbf4d29d82d45b5a6cfa
Anton Grouchtchak <goldtom1423@gmail.com>
2023-01-06 19:17:40 -0500

    Fix bug when updating from 2.2 to 2.3 [#188]
    Config Converter V3 assumed `device_groups` already exist in V2 configs.
    Add typehints.

--- server/libs/config_converter/config_converter_v3.py
+++ server/libs/config_converter/config_converter_v3.py
@@ -5,15 +5,19 @@ import logging
 
 class ConfigConverterV3(ConfigConverterBase):
     """
-    Config V3 updates the format used for groups (list -> dict).
+    Config V3 updates the format used for groups (`list` -> `dict`).
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.logger = logging.getLogger(__name__)
 
         self.from_version = 2
         self.to_version = 3
 
-    def upgrade(self, old_config):
+    def upgrade(self, old_config: dict) -> dict:
+        """
+        Upgrade config from version 2 to version 3.
+        Run all upgrade steps.
+        """
         self.logger.info("Upgrade config to version 3.")
 
         upgrade_step_1 = self.format_global_groups(old_config)
@@ -26,36 +30,46 @@ class ConfigConverterV3(ConfigConverterBase):
         self.logger.info("Config upgraded to version 3.")
         return new_config
 
-    def format_global_groups(self, old_config):
-        # Change old list format of global groups to dict.
-        global_groups = old_config["general_settings"]["device_groups"]
+    def format_global_groups(self, old_config: dict) -> dict:
+        """
+        Change old `list` format of global groups to `dict`.
+        """
+        global_groups = old_config["general_settings"].get("device_groups", [])
 
         if isinstance(global_groups, dict):
             return old_config
 
-        new_global_groups = {f"group_{i}": group for i, group in enumerate(global_groups)}
+        new_global_groups = {
+            f"group_{i}": group
+            for i, group in enumerate(global_groups)
+        }
+
         old_config["general_settings"]["device_groups"] = new_global_groups
         return old_config
 
-    def format_device_groups(self, old_config):
-        # Change old list format of device groups to dict.
-        global_groups = old_config["general_settings"]["device_groups"]
+    def format_device_groups(self, old_config: dict) -> dict:
+        """
+        Change old `list` format of device groups to `dict`.
+        """
+        global_groups = old_config["general_settings"]["device_groups"]  # Will exist after step 1.
 
         for device in old_config["device_configs"]:
-            device_groups = old_config["device_configs"][device]["device_groups"]
+            device_groups = old_config["device_configs"][device].get("device_groups", [])
 
             if isinstance(device_groups, dict):
                 continue
 
-            new_device_groups = dict()
-            for group_id, group in global_groups.items():
-                if group in device_groups:
-                    new_device_groups[group_id] = group
+            new_device_groups = {
+                group_id: group
+                for group_id, group in global_groups.items() if group in device_groups
+            }
 
             old_config["device_configs"][device]["device_groups"] = new_device_groups
         return old_config
 
-    def format_default_device_groups(self, old_config):
-        # Change old list format of default device groups to dict.
+    def format_default_device_groups(self, old_config: dict) -> dict:
+        """
+        Change old `list` format of default device groups to `dict`.
+        """
         old_config["default_device"]["device_groups"] = {}
         return old_config
